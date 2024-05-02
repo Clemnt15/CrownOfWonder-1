a4177bdcae40521cb850d6f62292d7e1a576ae34
Clemnt15 <ensuquetctout@gmail.com>
2024-05-02 10:08:12 +0200

    WorkingMainMenu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,11 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
-   {
-       
-   }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SCN_1"); // Remplacez "NomDeVotreSceneDeJeu" par le nom de la scène à charger pour commencer le jeu
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("OPTIONS"); // Remplacez "Options" par le nom de la scène contenant les options
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT"); // Affiche un message dans la console pour indiquer que le jeu est quitté
+        Application.Quit(); // Ferme l'application
+    }
 }
